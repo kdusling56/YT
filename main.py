2a10353e2a49c475c31548cb4e86d8b17219a841
kdusling56 <kdusling6@gmail.com>
2024-03-06 05:12:54 -0500

    Main framework of YT

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("first")
+import tkinter
+import customtkinter
+from pytube import YouTube
+
+# System Settings
+
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
+
+# Our app frame
+app = customtkinter.CTk()
+app.geometry("900x640")
+
+app.title("Youtube Downloader")
+
+# Run app
+app.mainloop()
